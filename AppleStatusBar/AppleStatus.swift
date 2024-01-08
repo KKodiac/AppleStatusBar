ab4ca1efb7424ab66ac08ab77616d4a72fa3a453
Seongmin Hong <seongmin.hong.dev@gmail.com>
2024-01-08 23:59:12 +0900

    🚀 Initial Commit

---
+++ AppleStatusBar/AppleStatus.swift
@@ -0,0 +1,8 @@
+//
+//  AppleStatus.swift
+//  AppleStatusBar
+//
+//  Created by Sean Hong on 1/8/24.
+//
+
+import Foundation
---
+++ AppleStatusBar/AppleStatusBarViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AppleStatusBarViewModel.swift
+//  AppleStatusBar
+//
+//  Created by Sean Hong on 1/8/24.
+//
+
+import Foundation
