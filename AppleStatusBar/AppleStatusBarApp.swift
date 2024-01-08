6548996706c54cdc4f62b3cd9a2529c12a5ade73
Seongmin Hong <seongmin.hong.dev@gmail.com>
2024-01-08 17:45:20 +0900

    Initial Commit

---
+++ AppleStatusBar/AppleStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStatusBarApp.swift
+//  AppleStatusBar
+//
+//  Created by Sean Hong on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleStatusBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleStatusBar
+//
+//  Created by Sean Hong on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
